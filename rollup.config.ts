dacd93a20d8fd167e7f4221ce8f51609a99e98c0
janriemer <janriemer@tutanota.de>
2018-10-12 11:25:17 +0200

    chore(bundle): remove bundling into test/public folder
    The test/public folder had been created for browser tests,
    but we are not doing any browser tests anymore.

--- rollup.config.ts
+++ rollup.config.ts
@@ -35,30 +35,5 @@ export default [
       // Resolve source maps to the original source
       sourceMaps(),
     ],
-  },
-  {
-    input: `src/index.ts`,
-    output: [
-      { file: "test/public/scrolldir-observable.umd.js", name: libraryName, format: 'umd', sourcemap: true }
-    ],
-    external: [],
-    watch: {
-      include: 'src/**',
-    },
-    plugins: [
-      // Allow json resolution
-      json(),
-      // Compile TypeScript files
-      typescript({ useTsconfigDeclarationDir: true }),
-      // Allow bundling cjs modules (unlike webpack, rollup doesn't understand cjs)
-      commonjs(),
-      // Allow node_modules resolution, so you can use 'external' to control
-      // which external modules to include in the bundle
-      // https://github.com/rollup/rollup-plugin-node-resolve#usage
-      resolve(),
-
-      // Resolve source maps to the original source
-      sourceMaps(),
-    ],
   }
 ]
